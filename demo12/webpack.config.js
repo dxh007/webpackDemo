9487cbb8dad51abe93f39b08addb747166524703
MR DU <d_xinhao@163.com>
2016-12-27 11:45:51 +0800

    exposing global variables .externals

---
+++ demo12/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports={
+	entry:'./main.jsx',
+	output:{
+		filename:'bundle.js'
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.js[x]?$/,
+				exclude:/node_modules/,
+				loader:'babel-loader',
+				query:{
+					presets:['es2015','react']
+				}
+			},
+		]
+	},
+	externals:{
+		//require('data') is external and avaliable
+		// on the global var data
+		'data':'data'
+	}
+};
